1000491a7e2109b94fb2963b929760fc06f5ee98
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-09-19 22:09:25 +0700

    Hardcode v2

--- config/db.go
+++ config/db.go
@@ -5,7 +5,7 @@ import (
 )
 
 func MongoConnect() (*mgo.Session, error) {
-	var session, err = mgo.Dial("mongodb://heroku_thr3t37f:d9qqnoegat1lam2dpkop0sekjm@ds235431.mlab.com:35431/heroku_thr3t37f")
+	var session, err = mgo.Dial("mongodb://heroku_ghdkzt3f:61o017lubhkdmddl2qc2jkiko1@ds137090.mlab.com:37090/heroku_ghdkzt3f")
 	if err != nil {
 		panic(err.Error())
 		return nil, err
