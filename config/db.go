21168a981f62047e8bb50343c3800de5e2db3a37
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-09-19 23:20:10 +0700

    Change env

--- config/db.go
+++ config/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func MongoConnect() (*mgo.Session, error) {
-	var session, err = mgo.Dial(os.Getenv("DB"))
+	var session, err = mgo.Dial(os.Getenv("MONGODB_URI"))
 	if err != nil {
 		panic(err.Error())
 		return nil, err
