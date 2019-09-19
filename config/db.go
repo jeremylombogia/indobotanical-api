3df6559c426de6f63a9f08e232952d5b179d7de2
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-09-19 22:05:51 +0700

    Hard code the db

--- config/db.go
+++ config/db.go
@@ -1,13 +1,11 @@
 package config
 
 import (
-	"os"
-
 	"gopkg.in/mgo.v2"
 )
 
 func MongoConnect() (*mgo.Session, error) {
-	var session, err = mgo.Dial(os.Getenv("DB"))
+	var session, err = mgo.Dial("mongodb://heroku_thr3t37f:d9qqnoegat1lam2dpkop0sekjm@ds235431.mlab.com:35431/heroku_thr3t37f")
 	if err != nil {
 		panic(err.Error())
 		return nil, err
