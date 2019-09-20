4c23b1741d7b4dc0ce021218ae32f5298c202616
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-09-20 14:52:33 +0700

    fix: remove vendor

--- vendor/github.com/jeremylombogia/indobotanical-api/product/payload.go
+++
@@ -1,9 +0,0 @@
-package product
-
-import "github.com/jeremylombogia/indobotanical-api/models"
-
-type Payload struct {
-	Data struct {
-		models.Products
-	} `json:"data"`
-}
--- vendor/github.com/jeremylombogia/indobotanical-api/transaction/payload.go
+++
@@ -1,9 +0,0 @@
-package transaction
-
-type Payload struct {
-	Data struct {
-		ProductID string `json:"productId"`
-		Amount    int    `json:"amount"`
-		PromoCode string `json:"promoCode"`
-	} `json:"data"`
-}
