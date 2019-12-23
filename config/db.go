f58d312c850c9af378fbf8f38a089b0aa0133271
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-12-23 16:45:47 +0700

    fix: fix db env

--- config/db.go
+++ config/db.go
@@ -1,13 +1,22 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"gopkg.in/mgo.v2"
 )
 
 func MongoConnect() *mgo.Session {
-	var sessionDb, err = mgo.Dial(os.Getenv("MONGODB_URI"))
+	host := fmt.Sprintf("%s", os.Getenv("DB_HOST"))
+	info := &mgo.DialInfo{
+		Addrs:    []string{host},
+		Database: os.Getenv("DB_NAME"),
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+	}
+
+	var sessionDb, err = mgo.DialWithInfo(info)
 	if err != nil {
 		panic(err.Error())
 	}
